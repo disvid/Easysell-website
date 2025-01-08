3fec632ff453b28a78fb8caeb551af1bf7ab509d
disvid <dishikavidhani@gmail.com>
2025-01-08 10:54:34 +0530

    added animation

--- resources/javascript/menubutton.js
+++ resources/javascript/menubutton.js
@@ -5,12 +5,18 @@ function show_hide() {
   const nav = document.querySelector('.main-nav');
 
   if (i === 1) {
-    nav.style.display = "none"; 
-    bars.classList.replace("fa-times", "fa-bars"); 
+    nav.classList.remove('active');
+    bars.classList.replace("fa-times", "fa-bars");
+    setTimeout(() => {
+      nav.style.display = "none";
+    }, 300);
     i = 0;
   } else {
-    nav.style.display = "flex"; 
-    bars.classList.replace("fa-bars", "fa-times"); 
+    nav.style.display = "flex";
+    setTimeout(() => {
+      nav.classList.add('active');
+    }, 10);
+    bars.classList.replace("fa-bars", "fa-times");
     i = 1;
   }
 }
